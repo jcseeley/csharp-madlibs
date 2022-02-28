bd8cad64a604b5422c77bfd62fb98c954c92e723
Will Greenberg <greenberg.will@gmail.com>
2022-02-28 11:53:26 -0800

    WIP linking css sheet

--- Madlibs/Controllers/HomeController.cs
+++ Madlibs/Controllers/HomeController.cs
@@ -10,13 +10,18 @@ namespace Madlibs.Controllers
     public ActionResult Form() { return View(); }
 
     [Route("/result")]
-    public ActionResult Madlib(string inputOne, string inputTwo, string inputThree, string inputFour)
+    public ActionResult Madlib(string inputOne, string inputTwo, string inputThree, string inputFour, string inputFive, string inputSix, string inputSeven, string inputEight, string inputNine)
     {
       UserInput myInput = new UserInput();
       myInput.InputOne = inputOne;
       myInput.InputTwo = inputTwo;
       myInput.InputThree = inputThree;
       myInput.InputFour = inputFour;
+      myInput.InputFive = inputFive;
+      myInput.InputSix = inputSix;
+      myInput.InputSeven = inputSeven;
+      myInput.InputEight = inputEight;
+      myInput.InputNine = inputNine;
       return View(myInput);
     }
   }
--- Madlibs/Models/UserInput.cs
+++ Madlibs/Models/UserInput.cs
@@ -10,6 +10,14 @@ namespace Madlibs.Models
 
     public string InputFour { get; set; }
 
+    public string InputFive { get; set; }
+
+    public string InputSix { get; set; }
+
+    public string InputSeven { get; set; }
+
+    public string InputEight { get; set; }
+    public string InputNine { get; set; }
 
   }
 }
